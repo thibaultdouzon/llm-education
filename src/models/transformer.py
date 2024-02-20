70233547c9f406c3784ccd46c21b22978a1cd85a
Thibault Douzon <douzont@gmail.com>
2024-02-21 00:25:55 +0100

    config class to store params

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -1,32 +1,53 @@
+from dataclasses import dataclass
 from functools import partial
 
 import torch
 import torch.nn as nn
 from einops import einsum, rearrange
 from jaxtyping import Float, Int
+from loguru import logger
+from transformers import AutoConfig, AutoModel
 from typeguard import typechecked
 
 
-class SinCosPositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, max_len: int = 512):
-        super().__init__()
-        self.d_model = d_model
-        self.max_len = max_len
+@dataclass
+class Config:
+    d_model: int
+    d_vocab: int
+    max_size: int
+    d_ff: int
+    n_heads: int
+    n_layers: int
+    is_causal: bool
+    dropout: float
 
-        self.pe = nn.Parameter(torch.zeros(1, max_len, d_model), requires_grad=False)
+    def __post_init__(self):
+        assert (
+            self.d_model % self.n_heads == 0
+        ), f"{self.d_model =} must be divisible by {self.n_heads = }"
+
+
+class SinCosPositionalEncoding(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.config = config
+
+        self.pe = nn.Parameter(
+            torch.zeros(1, config.max_len, config.d_model), requires_grad=False
+        )
 
     def init_weights(self):
-        pos = torch.arange(self.max_len, dtype=self.pe.dtype)
+        pos = torch.arange(self.config.max_len, dtype=self.pe.dtype)
         pos = pos.unsqueeze(-1) / (
-            10000 ** (torch.arange(0, self.d_model, 2) / self.d_model)
+            10000 ** (torch.arange(0, self.config.d_model, 2) / self.config.d_model)
         )
         print(torch.sin(pos[:10]))
         self.pe.data = rearrange(
             [torch.sin(pos), torch.cos(pos)],
             "a l d2 -> 1 l (d2 a)",
             a=2,
-            l=self.max_len,
-            d2=self.d_model // 2,
+            l=self.config.max_len,
+            d2=self.config.d_model // 2,
         )
 
     @typechecked
@@ -36,12 +57,11 @@ class SinCosPositionalEncoding(nn.Module):
 
 
 class DotProductAttention(nn.Module):
-    def __init__(self, is_causal: bool = False, dropout: float = 0.1):
+    def __init__(self, config: Config):
         super().__init__()
-        self.dropout = dropout
-        self.is_causal = is_causal
+        self.config = config
 
-        self.attn_dropout = nn.Dropout(dropout)
+        self.attn_dropout = nn.Dropout(config.dropout)
 
     @typechecked
     def forward(
@@ -55,7 +75,7 @@ class DotProductAttention(nn.Module):
         attn_weights = einsum(q, k, "b l2 h e, b l1 h e -> b h l2 l1") * scale
 
         attn_bias = torch.zeros(1, 1, len_q, len_k, dtype=q.dtype, device=q.device)
-        if self.is_causal:
+        if self.config.is_causal:
             attn_bias_msk = torch.ones(
                 1, 1, len_q, len_k, dtype=torch.bool, device=q.device
             ).tril(diagonal=0)
@@ -89,51 +109,46 @@ def merge_heads(
 
 
 class SelfAttention(nn.Module):
-    def __init__(
-        self, d_model: int, n_heads: int, is_causal: bool = False, dropout: float = 0.1
-    ):
+    def __init__(self, config: Config):
         super().__init__()
 
-        self.d_model = d_model
-        self.n_heads = n_heads
-        self.is_causal = is_causal
-        self.dropout = dropout
+        self.config = config
 
-        self.head_dim = d_model // n_heads
+        head_dim = config.d_model // config.n_heads
         assert (
-            self.head_dim * n_heads == d_model
-        ), f"{d_model =} must be divisible by {n_heads =}"
+            head_dim * config.n_heads == config.d_model
+        ), f"{config.d_model =} must be divisible by {config.n_heads =}"
 
-        self.qkv_proj = nn.Linear(d_model, 3 * d_model)
+        self.qkv_proj = nn.Linear(config.d_model, 3 * config.d_model)
 
-        self.attention = DotProductAttention(is_causal=False, dropout=dropout)
+        self.attention = DotProductAttention(config)
 
-        self.out_proj = nn.Linear(d_model, d_model)
-        self.out_dropout = nn.Dropout(dropout)
+        self.out_proj = nn.Linear(config.d_model, config.d_model)
+        self.out_dropout = nn.Dropout(config.dropout)
 
     @typechecked
     def forward(self, x: Float[torch.Tensor, "b l d"]) -> Float[torch.Tensor, "b l d"]:
 
         q, k, v = self.qkv_proj(x).chunk(3, dim=-1)
-        q_h, k_h, v_h = map(partial(split_heads, n_heads=self.n_heads), (q, k, v))
+        q_h, k_h, v_h = map(
+            partial(split_heads, n_heads=self.config.n_heads), (q, k, v)
+        )
 
         out_h = self.attention(q_h, k_h, v_h)
-        out = merge_heads(out_h, self.n_heads)
+        out = merge_heads(out_h, self.config.n_heads)
         out = self.out_proj(out)
         return self.out_dropout(out)
 
 
 class FeedForward(nn.Module):
-    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
+    def __init__(self, config: Config):
         super().__init__()
-        self.d_model = d_model
-        self.d_ff = d_ff
-        self.dropout = dropout
+        self.config = config
 
-        self.m_ff_proj = nn.Linear(d_model, d_ff)
+        self.m_ff_proj = nn.Linear(config.d_model, config.d_ff)
         self.act = nn.GELU()
-        self.ff_m_proj = nn.Linear(d_ff, d_model)
-        self.ff_dropout = nn.Dropout(dropout)
+        self.ff_m_proj = nn.Linear(config.d_ff, config.d_model)
+        self.ff_dropout = nn.Dropout(config.dropout)
 
     @typechecked
     def forward(self, x: Float[torch.Tensor, "b l d"]) -> Float[torch.Tensor, "b l d"]:
@@ -144,25 +159,13 @@ class FeedForward(nn.Module):
 
 
 class Block(nn.Module):
-    def __init__(
-        self,
-        d_model: int,
-        d_ff: int,
-        n_heads: int,
-        is_causal: bool = False,
-        dropout: float = 0.1,
-    ):
+    def __init__(self, config: Config):
         super().__init__()
-        self.d_model = d_model
-        self.d_ff = d_ff
-        self.n_heads = n_heads
-        self.is_causal = is_causal
-        self.dropout = dropout
-
-        self.attn = SelfAttention(d_model, n_heads, is_causal, dropout)
-        self.norm_attn = nn.LayerNorm(d_model)
-        self.ffn = FeedForward(d_model, d_ff, dropout)
-        self.norm_ffn = nn.LayerNorm(d_model)
+        self.config = config
+        self.attn = SelfAttention(config)
+        self.norm_attn = nn.LayerNorm(config.d_model)
+        self.ffn = FeedForward(config)
+        self.norm_ffn = nn.LayerNorm(config.d_model)
 
     @typechecked
     def forward(self, x: Float[torch.Tensor, "b l d"]) -> Float[torch.Tensor, "b l d"]:
@@ -173,30 +176,15 @@ class Block(nn.Module):
 class Transformer(nn.Module):
     def __init__(
         self,
-        d_model: int,
-        d_vocab: int,
-        d_ff: int,
-        n_heads: int,
-        n_layers: int,
-        is_causal: bool = False,
-        dropout: float = 0.1,
+        config: Config,
     ):
         super().__init__()
-        self.d_model = d_model
-        self.d_ff = d_ff
-        self.n_heads = n_heads
-        self.n_layers = n_layers
-        self.dropout = dropout
+        self.config = config
 
-        self.embedding = nn.Embedding(d_vocab, d_model)
-        self.positional_encoding = nn.Embedding(d_vocab, d_model)
+        self.embedding = nn.Embedding(config.d_vocab, config.d_model)
+        self.positional_encoding = nn.Embedding(config.max_size, config.d_model)
 
-        self.layers = nn.ModuleList(
-            [
-                Block(d_model, d_ff, n_heads, is_causal=is_causal, dropout=dropout)
-                for _ in range(n_layers)
-            ]
-        )
+        self.layers = nn.ModuleList([Block(config) for _ in range(config.n_layers)])
         self.apply(self._init_weights)
 
     def _init_weights(self, module: nn.Module):
@@ -216,3 +204,32 @@ class Transformer(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+
+    @classmethod
+    def from_pretrained(
+        cls,
+        model_name: str,
+    ):
+
+        config_hf = AutoConfig.from_pretrained(model_name)
+
+        config = dict(
+            d_model=config_hf.hidden_size,
+            d_vocab=config_hf.vocab_size,
+            max_size=config_hf.n_positions,
+            d_ff=config_hf.n_ctx,
+            n_heads=config_hf.n_head,
+            n_layers=config_hf.n_layer,
+            is_causal=False,
+            dropout=config_hf.attn_pdrop,
+        )
+
+        model_hf = AutoModel.from_pretrained(model_name)
+        logger.info(config_hf)
+        sd_hf = model_hf.state_dict()
+        for k, v in sd_hf.items():
+            logger.info(f"{k = } {v.shape = }")
+
+        model = cls(**config)
+
+        return model
